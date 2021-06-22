03fef620cf7fa20f4528131745782a9075a24733
theodore_mca <theodoreimonigie@gmail.com>
2021-06-22 12:13:32 -0500

    first commit

---
+++ database/migrations/2021_06_22_161445_create_discussions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscussionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discussions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('color')->default('#67hh00');
+            $table->bigInteger('section_id');
+            $table->boolean("answered")->default(false);
+            $table->boolean("pinned")->default(false);
+            $table->boolean("closed")->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discussions');
+    }
+}
